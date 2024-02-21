154ffe63b3d814bf9d5a644d59c46b36293e02f9
Volodymyr Hohunskyi <vgogunsky@griddynamics.com>
2024-02-21 12:42:25 +0200

    Added base structure

---
+++ AlgorithmVisualizer/AlgorithmVisualizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlgorithmVisualizerApp.swift
+//  AlgorithmVisualizer
+//
+//  Created by vgogunsky on 21.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct AlgorithmVisualizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
